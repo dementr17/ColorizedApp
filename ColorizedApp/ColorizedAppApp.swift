54eb03e18cef9cebff2b8791e2c2e02243651ae9
Dmitry <dementr17@yandex.ru>
2022-02-18 00:27:05 +0300

    first commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Дмитрий Чепанов on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
